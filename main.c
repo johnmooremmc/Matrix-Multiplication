09bc2e8404e2b305ec5c111741584a3dd82c1ae5
John Moore <84421728+johnmooremmc@users.noreply.github.com>
2022-01-11 16:59:13 +1300

    added angle calculation

--- main.c
+++ main.c
@@ -4,85 +4,77 @@
 int main()
 {
 
-    int a[3][3], b[3][3], result[3][3];
-    int arows, acolumns, brows, bcolumns;
+    int a[3][3], b[3][3];
+    int arows, bcolumns, dimension;
     int i, j, k;
-    int placeholder = 0;
+    int dot = 0;
+    int amag = 0, bmag = 0;
+    float anglerad, angledeg, magnitude;
 
-    printf("Enter the rows and columns of the matrix a: ");
-    scanf("%d %d", &arows, &acolumns);
+    printf("Dimensionality Degree: ");
+    scanf("%d", &dimension);
 
-    //printf("%d : %d \n", arows, acolumns);
+    printf("\n");
 
-    printf("Enter the elements of matrix a: \n");
-    for(i=0; i<arows; i++)
-    {
 
-     for(j=0; j<acolumns; j++)
-        {
+    arows = 1;
+    bcolumns = 1;
+
+    printf("Elements of A: \n");
+    for(i=0; i<arows; i++){
+        for(j=0; j<dimension; j++){
             scanf("%d", &a[i][j]);
-
         }
     }
 
-    for(i=0; i<arows; i++){
-        for(j=0; j<acolumns; j++) {
-            printf("%d ", a[i][j]);
-        }
         printf("\n");
-    }
 
-    printf("\n");
-
-    printf("Enter the rows and columns of the matrix b: ");
-    scanf("%d %d", &brows, &bcolumns);
-
-    if(brows != acolumns) {
-        printf("BREAK \n");
-    }
-    else {
-        printf("Enter the elements of matrix b: \n");
-        for(i=0; i<brows; i++) {
+        printf("Elements of B: \n");
+        for(i=0; i<dimension; i++){
             for(j=0; j<bcolumns; j++){
-                    scanf("%d", &b[i][j]);
-
+                scanf("%d", &b[i][j]);
             }
         }
-    }
 
-    for(i=0; i<brows; i++){
-        for(j=0; j<bcolumns; j++) {
-            printf("%d ", b[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-
-
-    for(i=0; i<arows; i++){
-        for(j=0; j<bcolumns; j++){
-            for(k=0; k<brows; k++){
-                placeholder += a[i][k] * b[k][j];
+         for(i=0; i<arows; i++){
+            for(j=0; j<bcolumns; j++) {
+                for(k=0; k<dimension; k++){
+                    dot += a[i][k] * b[k][j];
+                }
             }
-            result[i][j] = placeholder;
-            placeholder = 0;
         }
-    }
 
-
-
-    for(i=0; i<arows; i++){
-        for(j=0; j<bcolumns; j++) {
-            printf("%d ", result[i][j]);
-        }
         printf("\n");
-    }
 
 
-    // 2by2 printf("%d %d \n%d %d", a[0][0], a[0][1], a[1][0], a[1][1]);
+        printf("Matrix A: \n");
+         for(i=0; i<arows; i++){
+            for(j=0; j<dimension; j++) {
+                printf("%d ", a[i][j]);
+                amag += a[i][j];
+            }
+            printf("\n");
+        }
 
+        printf("\n");
+
+         printf("Matrix B: \n");
+         for(i=0; i<dimension; i++){
+            for(j=0; j<bcolumns; j++) {
+                printf("%d ", b[i][j]);
+                bmag += b[i][j];
+            }
+            printf("\n");
+        }
+
+        magnitude = sqrt(amag) * sqrt(bmag);
+        anglerad = acos(dot/magnitude);
+        angledeg = 57.2957795*anglerad;
+
+
+        printf("\n");
+        printf("Dot product: %d \n", dot);
+        printf("Angle: %f : %f \n", anglerad, angledeg);
 
     return 0;
 }
-
